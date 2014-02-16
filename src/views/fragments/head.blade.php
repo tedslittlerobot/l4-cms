f25f60ed67e57327924e74c1f3ef8525ffbf47e3
Stef Horner <shorner@wearearchitect.com>
2014-02-16 15:14:14 +0000

    add head fragment

---
+++ src/views/fragments/head.blade.php
@@ -0,0 +1,22 @@
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+
+	@section('meta')
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		@if( isset($meta) )
+			@foreach( $meta as $name => $content )
+				<meta name="{{{ $name }}}" content="{{{ $content }}}">
+			@endforeach
+		@endif
+	@show
+
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/gumby/2.5.11/css/gumby.css">
+
+	@section( 'styles' )
+		{{-- styles --}}
+	@show
+</head>
