68b997b9735371c0e8cf84a8ac1f9f92f47dd185
Stef Horner <stef@tedslittlerobot.com>
2014-02-22 12:09:00 +0000

    add navbar to admin view

--- src/views/admin.blade.php
+++ src/views/admin.blade.php
@@ -1,1 +1,15 @@
 @extends('l4-cms::layout')
+
+@section('header')
+	@section('header-nav')
+		<nav class="navbar" role="navigation">
+			<h1 class="four columns logo">
+				<a href="{{ route('admin') }}">
+					{{ $title or 'Velox' }}
+				</a>
+			</h1>
+			{{ Menu::menu('admin-header-nav')->render() }}
+		</nav>
+	@show
+	@yield('breadcrumbs')
+@stop
